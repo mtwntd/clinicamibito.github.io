c7a4206269de4340dd0f4fa13101eb6da204981d
Tomas <josetomascampos2106@gmail.com>
2025-11-24 18:41:19 -0300

    Delete ClinicaMiBiTo directory

--- ClinicaMiBiTo/ClinicaMiBiTo/appclinica/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class AppclinicaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appclinica'
-
-    def ready(self):
-        import appclinica.signals
